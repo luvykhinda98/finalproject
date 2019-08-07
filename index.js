bf619563a60f0bf8cefb48b32ac0ace49085a307
Lovepreet Singh <747629@student.tor.loc>
2019-08-07 17:16:37 -0400

    forth

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-var express = require('express');
+var express = require('express')
 var app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(function(req, res, next) {
+    console.log("I am in the middle.");
+    console.log(req.body.username);
+    console.log(req.body.password);
+    next();
+
+})
 
 app.get('/', function(req, res) {
-    res.send('Hello World')
+    res.send('Get Sent')
 })
-app.listen(3000)
+app.post('/', function(req, res) {
+    console.log(req.body);
+    res.send('Post Sent')
+})
+app.put('/', function(req, res) {
+
+    res.send('PUT Sent')
+})
+
+app.delete('/', function(req, res) {
+    ServiceUIFrameContext.send('Delete Sent')
+})
+
+app.listen(3000);
